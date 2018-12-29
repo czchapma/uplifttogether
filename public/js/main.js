ee8f5725397545fa253fc8399e2e9d95d6d71605
Christine <christine_chapman@brown.edu>
2018-12-29 13:48:37 -0500

    Initial Commit

---
+++ public/js/main.js
@@ -0,0 +1,199 @@
+;(function () {
+
+	'use strict';
+
+
+
+	// iPad and iPod detection
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function(){
+	    return (
+			(navigator.platform.indexOf("iPhone") != -1) ||
+			(navigator.platform.indexOf("iPod") != -1)
+	    );
+	};
+
+	// Carousel Feature Slide
+var owlCrouselFeatureSlide = function() {
+
+	var owl = $('.owl-carousel');
+
+	owl.on('initialized.owl.carousel change.owl.carousel',function(elem){
+		var current = elem.item.index;
+		$(elem.target).find(".owl-item").eq(current).find(".to-animate").removeClass('fadeInUp animated');
+		$(elem.target).find(".owl-item").eq(current).find(".to-animate-2").removeClass('fadeInUp animated');
+
+	});
+	owl.on('initialized.owl.carousel changed.owl.carousel',function(elem){
+		setTimeout(function(){
+			var current = elem.item.index;
+			$(elem.target).find(".owl-item").eq(current).find(".to-animate").addClass('fadeInUp animated');
+		}, 700);
+		setTimeout(function(){
+			var current = elem.item.index;
+			$(elem.target).find(".owl-item").eq(current).find(".to-animate-2").addClass('fadeInUp animated');
+		}, 900);
+		});
+
+
+	owl.owlCarousel({
+		items: 1,
+			loop: true,
+			margin: 0,
+			responsiveClass: true,
+			nav: true,
+			dots: true,
+			autoHeight: true,
+			smartSpeed: 500,
+			autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true,
+			navText: [
+				"<i class='icon-arrow-left2 owl-direction'></i>",
+				"<i class='icon-arrow-right2 owl-direction'></i>"
+			]
+	});
+
+};
+
+
+	// Main Menu Superfish
+	var mainMenu = function() {
+
+		$('#fh5co-primary-menu').superfish({
+			delay: 0,
+			animation: {
+				opacity: 'show'
+			},
+			speed: 'fast',
+			cssArrows: true,
+			disableHI: true
+		});
+
+	};
+
+	// Parallax
+	var parallax = function() {
+		$(window).stellar();
+	};
+
+
+	// Offcanvas and cloning of the main menu
+	var offcanvas = function() {
+
+		var $clone = $('#fh5co-menu-wrap').clone();
+		$clone.attr({
+			'id' : 'offcanvas-menu'
+		});
+		$clone.find('> ul').attr({
+			'class' : '',
+			'id' : ''
+		});
+
+		$('#fh5co-page').prepend($clone);
+
+		// click the burger
+		$('.js-fh5co-nav-toggle').on('click', function(){
+
+			if ( $('body').hasClass('fh5co-offcanvas') ) {
+				$('body').removeClass('fh5co-offcanvas');
+			} else {
+				$('body').addClass('fh5co-offcanvas');
+			}
+			// $('body').toggleClass('fh5co-offcanvas');
+
+		});
+
+		$('#offcanvas-menu').css('height', $(window).height());
+
+		$(window).resize(function(){
+			var w = $(window);
+
+
+			$('#offcanvas-menu').css('height', w.height());
+
+			if ( w.width() > 769 ) {
+				if ( $('body').hasClass('fh5co-offcanvas') ) {
+					$('body').removeClass('fh5co-offcanvas');
+				}
+			}
+
+		});
+
+	}
+
+
+
+	// Click outside of the Mobile Menu
+	var mobileMenuOutsideClick = function() {
+		$(document).click(function (e) {
+	    var container = $("#offcanvas-menu, .js-fh5co-nav-toggle");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('fh5co-offcanvas') ) {
+				$('body').removeClass('fh5co-offcanvas');
+			}
+	    }
+		});
+	};
+
+
+	// Animations
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							el.addClass('fadeInUp animated');
+							el.removeClass('item-animate');
+						},  k * 200, 'easeInOutExpo' );
+					});
+
+				}, 100);
+
+			}
+
+		} , { offset: '85%' } );
+	};
+
+
+	// Document on load.
+	$(function(){
+		mainMenu();
+		owlCrouselFeatureSlide();
+		parallax();
+		offcanvas();
+		mobileMenuOutsideClick();
+		contentWayPoint();
+
+
+	});
+
+
+}());
+
+$(document).ready(function(){
+    // Add minus icon for collapse element which is open by default
+    $(".collapse.in").each(function(){
+        $(this).siblings(".panel-heading").find(".glyphicon").addClass("glyphicon-minus").removeClass("glyphicon-plus");
+    });
+
+    // Toggle plus minus icon on show hide of collapse element
+    $(".collapse").on('show.bs.collapse', function(){
+        $(this).parent().find(".glyphicon").removeClass("glyphicon-plus").addClass("glyphicon-minus");
+    }).on('hide.bs.collapse', function(){
+        $(this).parent().find(".glyphicon").removeClass("glyphicon-minus").addClass("glyphicon-plus");
+    });
+});
